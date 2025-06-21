275b79139b278a3fbb86abf740104257c60dafdb
rayzen2002 <emanuelzsmj@gmail.com>
2025-06-21 15:05:41 -0300

    Fix: Erro mesage on /migration

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -4,7 +4,7 @@ import database from "infra/database.js"
 export default async function migrations(request , response){
   const allowedMethods = ["GET","POST"];
   if(!allowedMethods.includes(request.method)){
-    return responde.status(405).json({
+    return response.status(405).json({
       error: `Method "${request.method}" not allowed`,
     });
   }
