d60dc4a7eb6a6edd9076adbfed2d90501acf0779
rayzen2002 <emanuelzsmj@gmail.com>
2025-06-21 14:55:27 -0300

    Changing error message on `/migrations`

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -4,7 +4,7 @@ import database from "infra/database.js"
 export default async function migrations(request , response){
   const allowedMethods = ["GET","POST"];
   if(!allowedMethods.includes(request.method)){
-    return responde.status(405).end({
+    return responde.status(405).json({
       error: `Method "${request.method}" not allowed`,
     });
   }
