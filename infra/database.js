6a5b620f5d54afcf1bb3d6165b5f7ca5d8a71d9a
rayzen2002 <emanuelzsmj@gmail.com>
2025-12-23 23:43:01 -0300

    Fix `prettier` style linting

--- infra/database.js
+++ infra/database.js
@@ -1,34 +1,33 @@
 import { Client } from "pg";
 
-async function query(queryObject){
-  let client 
+async function query(queryObject) {
+  let client;
   try {
-    client = await getNewClient()
-    const result = await client.query(queryObject)
-  
-  return result
+    client = await getNewClient();
+    const result = await client.query(queryObject);
+
+    return result;
   } catch (error) {
-    console.error(error)
-  } finally{
-    await client.end()
+    console.error(error);
+  } finally {
+    await client.end();
   }
-  
 }
 
-async function getNewClient(){
+async function getNewClient() {
   const client = new Client({
-    host:  process.env.POSTGRES_HOST,
-    port:  process.env.POSTGRES_PORT,
-    user:  process.env.POSTGRES_USER,
-    database:  process.env.POSTGRES_DB,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === 'production' ? true : false
-  })
-  await client.connect()
-  return client
+    ssl: process.env.NODE_ENV === "production" ? true : false,
+  });
+  await client.connect();
+  return client;
 }
 
 export default {
   query,
-  getNewClient
-}
+  getNewClient,
+};
