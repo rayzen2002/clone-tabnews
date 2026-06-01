85c81735873afa1410ca09121afbe11deed9ea0f
rayzen2002 <emanuelzsmj@gmail.com>
2026-05-31 23:35:55 -0300

    refactor: user `orchestrator.clearDatabase()` in tests

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,10 +1,9 @@
-import database from "infra/database.js";
 import { describe } from "node_modules/eslint/lib/rule-tester/flat-rule-tester";
 import orchestrator from "tests/orchestrator.js";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
-  await database.query("drop schema public cascade; create schema public");
+  await orchestrator.clearDatabase();
 });
 
 describe("POST /api/v1/migrations", () => {
